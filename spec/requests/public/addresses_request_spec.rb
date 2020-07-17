461d1f04c398653d3ebc8a95bb8e576b52014807
kenji <tora169mm@gmail.com>
2020-07-17 17:46:17 +0900

    [fix] テストケースの修正

--- spec/requests/public/addresses_request_spec.rb
+++ spec/requests/public/addresses_request_spec.rb
@@ -4,37 +4,36 @@ RSpec.describe "Public::Addresses", type: :request do
 
   describe "GET /index" do
     it "returns http success" do
-      get "public_addresses_path"
+      get public_addresses_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /create" do
     it "returns http success" do
-      get "public_addresses"
+      get public_addresses_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /edit" do
     it "returns http success" do
-      get "edit_public_address"
+      get edit_public_address_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /update" do
     it "returns http success" do
-      get "public_address"
+      get public_address_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /destroy" do
     it "returns http success" do
-      get "public_address"
+      get public_address_path
       expect(response).to have_http_status(:success)
     end
   end
-
 end
