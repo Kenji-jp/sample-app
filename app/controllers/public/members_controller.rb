d098b910c83b91694c9b62e6679e4b8882c35338
kenji <tora169mm@gmail.com>
2020-07-17 09:44:06 +0900

    [fix]ユーザー情報の編集機能の改修

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -9,7 +9,9 @@ class Public::MembersController < ApplicationController
   end
 
   def update
-
+  	@member = current_member
+  	@member.update(member_params)
+  	redirect_to public_members_path
   end
 
   def withdrawal
@@ -22,7 +24,7 @@ class Public::MembersController < ApplicationController
 
  private
   def member_params
-
+    params.require(:member).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :withdrawal_status, :email)
   end
 
 end
