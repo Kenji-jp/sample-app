1938d23aab577bd99d902a19b37318dd978a6a84
kenji <tora169mm@gmail.com>
2020-07-18 10:27:56 +0900

    [add]配送先住所のCRUD機能の追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -6,14 +6,30 @@ class Public::AddressesController < ApplicationController
   end
 
   def create
+    @address = Address.new(address_params)
+  	@address.member_id = current_member.id
+  	@address.save
+  	redirect_to public_addresses_path
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+  	@address = Address.find(params[:id])
+  	@address.update(address_params)
+  	redirect_to public_addresses_path
   end
 
   def destroy
+  	@address = Address.find(params[:id])
+  	@address.destroy
+  	redirect_to public_addresses_path
+  end
+
+  private
+  def address_params
+  	params.require(:address).permit(:name, :member_id, :shipping_address, :postal_code)
   end
 end
